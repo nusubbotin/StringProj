ca80e7b3e43d2cb8420b0243922c9d6f1fab800d
nusubbotin <nu.subbotin@ya.ru>
2022-10-01 08:53:58 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+    }
 
+    private static void task1() {
+        System.out.println("Задание1:");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+
+        System.out.println("ФИО сотрудника — " + fullName);
     }
 }
